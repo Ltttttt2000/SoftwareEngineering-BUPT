eea23e780cf3560e06b95781bc8403c8d3eac767
S0range <1725559150@qq.com>
2021-04-27 21:52:17 +0800

    2021-04-27 Sorange update

--- core/src/fxml/StorePageController.java
+++ core/src/fxml/StorePageController.java
@@ -4,6 +4,8 @@ import com.iot.g89.GUIDriver;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -32,7 +34,27 @@ public class StorePageController  implements Initializable {
 
     public Button drawButton(String userId){
         Button button = new Button();
+        BorderPane buttonPane = new BorderPane();
+
+        Label userIdLabel = new Label("ID: " + userId);
+
+        Label ageTextLabel = new Label("Age: ");
+        Label sexTextLabel = new Label("Sex: ");
+        Label userTypeTextLabel = new Label("User Type: ");
+        Label userLevelTextLabel = new Label("User Level: ");
+
+        Label ageLabel = new Label();
+        Label sexLabel = new Label();
+        Label userTypeLabel = new Label();
+        Label userLevelLabel = new Label();
+
+        buttonPane.setLeft(userIdLabel);
+
+
+        buttonPane.setPrefSize(768, 84);
         button.setPrefSize(768, 84);
+
+        button.setGraphic(buttonPane);
 
         return button;
     }
