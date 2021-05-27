f55bd822011bd9024d0e4b9afbdb823a2f87a758
ly129 <zhg422@163.com>
2021-05-27 11:10:31 +0800

    2021/5/27 聚合表 ly129

---
+++ core/src/com/iot/g89/PurchaseInstructor.java
@@ -0,0 +1,41 @@
+package com.iot.g89;
+
+import java.util.ArrayList;
+
+public class PurchaseInstructor {
+
+    public static ArrayList<Object> aggregateByClient(String userId){
+        ArrayList<Object> returnList = new ArrayList<>();
+
+        String filePath = "./core/src/csv/PurchaseInstructor.csv";
+        String[] readAll = {"*"};
+        ArrayList<String[]> allList = FileUtils.readCSV(filePath, readAll);
+
+        ArrayList<String> idList = new ArrayList<>();
+
+//        aggregate by client, return instructor
+        if(userId.charAt(0) == 'C'){
+            for(String[] para : allList){
+                if(para[1].equals(userId))
+                    idList.add(para[0]);
+                }
+            for(String Id : idList){
+                Instructor i = new Instructor(Id);
+                returnList.add(i);
+            }
+//        aggregate by instructor, return client
+        }else{
+            for(String[] para : allList){
+                if(para[0].equals(userId))
+                    idList.add(para[1]);
+            }
+            for(String Id : idList) {
+                Client c = new Client(Id);
+                returnList.add(c);
+            }
+        }
+        return returnList;
+    }
+
+
+}
