bcc160d048c87b032a3e025437bb8e8f8061359d
S0range <1725559150@qq.com>
2021-05-30 01:44:57 +0800

    2021-05-30 booking界面还差一点点

--- core/src/fxml/CourseBookingPageController.java
+++ core/src/fxml/CourseBookingPageController.java
@@ -1,6 +1,8 @@
 package fxml;
 
 import com.iot.g89.GUIDriver;
+import com.iot.g89.Instructor;
+import com.iot.g89.Live;
 import com.iot.g89.SceneTransform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,6 +11,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -16,18 +19,24 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
-import java.util.Random;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class CourseBookingPageController implements Initializable {
     @FXML
     private VBox instructorVBox;
+    @FXML
+    private VBox liveVBox;
+    @FXML
+    private Label note;
+
+    private ArrayList<Object> bookedLives = new ArrayList<Object>();
 
     private GUIDriver driver;
     private Scene thisScene;
     private Scene lastScene;
 
-    private String userId;
+    private String curUserId;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -35,29 +44,35 @@ public class CourseBookingPageController implements Initializable {
 
     public void initData(String userId, Scene thisScene, Scene lastScene, GUIDriver driver) {
         this.driver = driver;
-        this.userId = userId;
+        this.curUserId = userId;
         this.thisScene = thisScene;
         this.lastScene = lastScene;
         listInstructors();
+        note.setText("");
+        liveVBox.getChildren().clear();
     }
 
     private void listInstructors(){
-        String[] instructors = getAllInstructorsId();
-        for(String i:instructors){
-            instructorVBox.getChildren().add(drawInstructorButton(i));
+        ArrayList<Object> instructors = driver.select("Instructor", curUserId);
+        instructorVBox.getChildren().clear();
+        for(Object i:instructors){
+            instructorVBox.getChildren().add(drawInstructorButton((Instructor) i));
         }
     }
 
-    private String[] getAllInstructorsId() {
-        String[] strs = {"I1001", "I1002", "I1003", "I1004", "I1005", "I1006"};
-        return strs;
+    public void listLives(String instructorId){
+        ArrayList<Object> lives = driver.select("Live InstructorId=" + instructorId);
+        bookedLives = driver.select("Live", curUserId);
+        liveVBox.getChildren().clear();
+        for(Object l:lives){
+            if(bookedLives.contains(l))
+                liveVBox.getChildren().add(drawLiveButton((Live) l, true));
+            else
+                liveVBox.getChildren().add(drawLiveButton((Live) l, false));
+        }
     }
 
-    public void showBookingList(){
-        System.out.println("yes");
-    }
-
-    public Button drawInstructorButton(String userId){
+    public Button drawInstructorButton(Instructor user){
         StackPane imagePane = new StackPane();
 
         Image normalBorder = new Image("file:core/src/imgs/normalborder.png", 60, 60, false, false);
@@ -74,15 +89,13 @@ public class CourseBookingPageController implements Initializable {
         BorderPane buttonPane = new BorderPane();
         BorderPane centerPane = new BorderPane();
 
-        Label userIdLabel = new Label("ID: " + userId);
-
-        Random r = new Random();
-
-        String sex = driver.getSex(userId);
-        String age = driver.getAge(userId);
+        String userId = user.getUserId();
+        String sex = user.getSex();
+        String age = String.valueOf(user.getAge());
         String userType = driver.getUsertype(userId);
-        String userLevel = driver.getUserLevel(userId);
+        String userLevel = user.getUserLevel();
 
+        Label userIdLabel = new Label("ID: " + userId);
         Label levelLabel = new Label(userType + " Level: " + userLevel);
 
         // bigger labels
@@ -130,8 +143,82 @@ public class CourseBookingPageController implements Initializable {
         button.setGraphic(buttonPane);
 
         button.setOnAction(e ->{
-            showBookingList();
+            listLives(userId);
         });
+
+        return button;
+    }
+
+    public Button drawLiveButton(Live live, Boolean booked) {
+        ImageView stackImg = new ImageView("file:core/src/imgs/tickImg.png");
+
+        Button button = new Button();
+
+        BorderPane buttonPane = new BorderPane();
+        BorderPane leftPane = new BorderPane();
+        BorderPane rightPane = new BorderPane();
+
+        String[] dateTime = live.getDate().split(" ");
+        String liveId = live.getLiveId();
+        String number = String.valueOf(live.getNumber());
+
+        Label dateLabel = new Label(dateTime[0]);
+        Label timeLabel = new Label("Time: " + dateTime[1] + ":00");
+        Label liveIdLabel = new Label("Live ID: " + liveId);
+        Label numberLabel = new Label(number + "/20");
+
+        String style = "-fx-font-family: Arial; -fx-text-fill: white; -fx-font-size: 12px;";
+
+        button.setStyle("-fx-background-color: rgba(0,0,0,0.3);");
+        button.setTooltip(new Tooltip(live.getDescription()));
+        dateLabel.setStyle("-fx-font-family: 'Arial Black'; -fx-text-fill: white; -fx-font-size: 18px;");
+        timeLabel.setStyle(style);
+        liveIdLabel.setStyle(style);
+        numberLabel.setStyle(style);
+
+        // Layout in button
+        leftPane.setTop(dateLabel);
+        leftPane.setBottom(timeLabel);
+
+        rightPane.setLeft(stackImg);
+        rightPane.setRight(numberLabel);
+
+        buttonPane.setLeft(leftPane);
+        buttonPane.setCenter(liveIdLabel);
+        buttonPane.setRight(rightPane);
+
+        BorderPane.setAlignment(liveIdLabel, Pos.BOTTOM_CENTER);
+
+        button.setPadding(new Insets(10));
+        button.setGraphic(buttonPane);
+
+        // Button and pane size
+        rightPane.setPrefSize(55, 42);
+        leftPane.setPrefSize(122, 42);
+        buttonPane.setPrefSize(285, 42);
+        button.setPrefSize(305, 62);
+
+        stackImg.setVisible(booked);
+
+        button.setOnAction(e ->{
+            switch(driver.purchaseOrReserve(liveId)){
+                case -1:
+                    note.setText("No such instructor/video/live"); break;
+                case -2:
+                    note.setText("Repeat reserving"); break;
+                case -3:
+                    break;
+                case -4:
+                    note.setText("You are not his/her student"); break;
+                case -5:
+                    note.setText("The class you selected is full !"); break;
+                case 1:
+                    stackImg.setVisible(true);
+                    note.setText("");
+                    break;
+            }
+        });
+
 
         return button;
     }
